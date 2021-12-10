53fd97a5ffae12e686896523fcc0f7a57f721c52
Nicolas <72466959+nmoreno1498@users.noreply.github.com>
2021-12-10 15:19:43 -0300

    Add project files.

---
+++ ActorsRazorPages/Models/Data.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActorsRazorPages.Models
+{
+    public class Data : IData
+    {
+        public List<Actor> ActorsList { get; set; }
+
+        public List<Actor> ActorsInitializeData()
+        {
+            ActorsList = new List<Actor>()
+            {
+                new Actor(){Id = 1,FirstName="Kelly",LastName="Bailey" ,KnownFor="Kelly the great",OscarWinner=true,ImageName="/elrenacido.jpg"},
+            };
+
+            return ActorsList;
+        }
+
+       
+    }
+}
